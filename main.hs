bda9e0c32c690bb5d34950ac80a5df117fa1fd7d
joaoneto9 <joaonetocg1@gmail.com>
2026-01-13 11:30:22 -0300

    feat: implementing the sum Cards fucntion with the rule of the Aces values, correcting when the total value when it is bigger then 21, turning the Aces values to 1 value until the total is less then 21

--- main.hs
+++ main.hs
@@ -1,18 +1,38 @@
+import qualified Data.Map.Strict as HM
+import Text.Read (readMaybe)
+import Data.IntMap (adjust)
+
 split :: Char -> String -> [String]
 split _ [] = [""]
 split sep (c:cs) 
     | c == sep = "" : rest
     | otherwise = (c : head rest) : tail rest
-    where 
-        rest = split sep cs
+    where
+        rest = split sep cs 
+
+getStrValue :: String -> Int
+getStrValue "A" = 11 
+getStrValue s = case (readMaybe s :: Maybe Int) of
+    Just n -> n
+    Nothing -> 10 -- se for uma letra, retorna 10 -> ps: o "A" deve ser tratado de maneira especial no algoritmo 
+
+sumCards :: [String] -> Int
+sumCards cards = 
+    let values = map getStrValue cards
+        total = sum values 
+        aces = length (filter (== "A") cards)
+    in adjustAces total aces 
+
+adjustAces :: Int -> Int -> Int
+adjustAces total aces
+  | total <= 21 || aces == 0 = total -- ajusta o valor total, enquanto o "A" valer 11 
+  | otherwise = adjustAces (total - 10) (aces - 1)
 
 main :: IO()
 main = do 
     putStrLn "Insira as cartas do Jogador: (separe os valores por ' ')"
     usersCardsInput <- getLine
-    let usersCards = split ' ' usersCardsInput -- arrays de valores 
-    putStrLn "Insira a carta do Dealer: "
-    dealersCard <- getLine -- string unica (apenas um valor)
-    -- print apenas para não ter erro de compilação 
-    print usersCards
-    print dealersCard
+    let usersCards = split ' ' usersCardsInput -- arrays de valores
+    -- print apenas para não ter erro de compilação
+    let sumValues = sumCards usersCards 
+    print sumValues
