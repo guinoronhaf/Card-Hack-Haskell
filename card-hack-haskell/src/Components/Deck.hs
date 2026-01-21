c3378ff6fcb79c4c77bc815fbbc7e4445e46c9ba
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2026-01-21 14:54:17 -0300

    feat 2: writing on ProbAlgorithm.file

--- card-hack-haskell/src/Components/Deck.hs
+++ card-hack-haskell/src/Components/Deck.hs
@@ -1,15 +1,21 @@
-module Components.Deck (generateDeck, removeCard, getValue) where -- exportando as funções que devem ser utilizadas no projeto
+-- exportando as funções que serão utilizadas no projeto
+module Components.Deck (generateDeck, removeCard, getValue) where 
 
-import qualified Data.Map as Map -- importando Data.Map
-import Data.Maybe (isJust, fromJust) -- importando funções para trabalhar com Maybe, Just e Nothing
+-- importando Data.Map para trabalhar com mapas
+import qualified Data.Map as Map
+-- importanto Data.Maybe para trabalhar com os tipos Maybe, Just e Nothing
+import Data.Maybe (isJust, fromJust) 
 
-cardsList :: [String] -- lista com todos os ssímbolos de cartas presentes em um baralho convencional
+-- lista com todas as cartas (símbolos) presentes em um baralho convencional
+cardsList :: [String]
 cardsList = (map (\n -> show n) [2..10]) ++ ["J", "K", "Q", "A"]
 
-generateDeck :: Map.Map String Int -- geração de um mapa que determina a existência de 4 cartas para cada símbolo
+-- geração que uma lista de tuplas que associam cada carta à sua quantidade inicial no baralho (4)
+generateDeck :: Map.Map String Int 
 generateDeck = Map.fromList (map (\x -> (x, 4)) cardsList)
 
-removeCard :: String -> Map.Map String Int -> Map.Map String Int -- remove uma carta específica do baralho, atualizando o mapa
+-- remolção de um carta específica do baralho, atualizando o mapa
+removeCard :: String -> Map.Map String Int -> Map.Map String Int 
 removeCard key deckMap = do
 	let f k v = if k == key && v /= 0 then
 				   v - 1
@@ -17,7 +23,8 @@ removeCard key deckMap = do
 				   v
 	Map.adjustWithKey f key deckMap
 
-getValue :: String -> Map.Map String Int -> Int -- retorna a quantidade de cartas dado um símbolo específico
+-- retorna a quantidade de cartas de um determinado símbolo no baralho
+getValue :: String -> Map.Map String Int -> Int 
 getValue key deckMap = do
 	let value = Map.lookup key deckMap
 	if isJust value then
--- card-hack-haskell/src/Components/ProbAlgorithm.hs
+++ card-hack-haskell/src/Components/ProbAlgorithm.hs
@@ -1,4 +1,21 @@
 module Components.ProbAlgorithm (testFunc) where
 
+import qualified Data.Map as Map -- trabalhar com mapas
+import Data.Ratio as Ratio -- trabalhar precisamente com números em ponto flutuante
+import Components.Deck as Deck -- instanciar o mapa que representa o baralho
+import Util.AuxiliarFunctions as Aux -- utilizar funções auxiliares para compor o jogo
+
+isBlackJack :: [String] -> Bool
+isBlackJack cards = (Aux.sumCards cards) == 21
+
+isOverflow :: [String] -> Bool
+isOverflow cards = (Aux.sumCards cards) > 21
+
+-- blackjackProb :: [String] -> Map.Map String Int -> Ratio
+
+-- underflowProb :: [String] -> Map.Map String Int -> Ratio
+
+-- overFlowProb :: [String] -> Map.Map String Int -> Ratio
+
 testFunc :: String
 testFunc = "oi"
