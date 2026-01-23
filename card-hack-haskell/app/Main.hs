89ba497da9ebd145ec89e8866f2920f77037ac84
guinoronhaf <guilhermenoronhaf@gmail.com>
2026-01-23 10:43:01 -0300

    feat: finishing ProbAlgorithm.hs

--- card-hack-haskell/app/Main.hs
+++ card-hack-haskell/app/Main.hs
@@ -1,57 +1,43 @@
--- import Util.AuxiliarFunctions (setUpCards, sumCards)
---
--- import qualified Data.Map as Map
--- import Data.Char (toUpper)
---
--- main :: IO ()
--- main = do
---     gameLoop
---
--- gameLoop :: IO ()
--- gameLoop = do
---     putStrLn "Digite as suas cartas (separadas por ''):"
---     usersCard <- getLine
---
---     putStrLn "Digite a carta do Dealer:"
---     dealersCard <- getLine
---
---     let mapCards = setUpCards [usersCard, dealersCard]
---
---     -- call do algoritmo (Funcionalidade 2)
---
---     -- mostrar as possibilidades (Funcionalidade 4)
---     
---     -- solicitair reinicio de programa (Fucnionalidade 5)
---     restartButton
---
--- restartButton :: IO ()
--- restartButton = do
---     putStrLn "Deseja reiniciar o programa? (Y / N)"
---     input <- getLine
---
---     case restartCases input of 
---         'Y' -> gameLoop
---         'R' -> restartButton
---         'N' -> putStrLn "Fim do programa, obrigado pela preferência. Ass: CARD-HACK"
---
---
--- restartCases :: String -> Char
--- restartCases input = 
---     case (map toUpper input) of 
---         "Y" -> 'Y' 
---         "N" -> 'N'
---         _   -> 'R' -- input invalido, refazer
+import Util.AuxiliarFunctions (setUpCards, sumCards)
 
-import Components.Deck as Deck
-import Components.ProbAlgorithm as Prob
 import qualified Data.Map as Map
-import Data.Maybe (fromJust)
+import Data.Char (toUpper)
 
 main :: IO ()
 main = do
-	let m = Deck.generateDeck
-	let uCards = ["3"]
-	let dCards = ["Q", "5"]
-	let m1 = Deck.removeCards dCards (Deck.removeCards uCards m)
-	let p = Prob.underflowProb uCards 15 m1
-	print(p)
+    gameLoop
+
+gameLoop :: IO ()
+gameLoop = do
+    putStrLn "Digite as suas cartas (separadas por ''):"
+    usersCard <- getLine
+
+    putStrLn "Digite a carta do Dealer:"
+    dealersCard <- getLine
+
+    let mapCards = setUpCards [usersCard, dealersCard]
+
+    -- call do algoritmo (Funcionalidade 2)
+
+    -- mostrar as possibilidades (Funcionalidade 4)
+    
+    -- solicitair reinicio de programa (Fucnionalidade 5)
+    restartButton
+
+restartButton :: IO ()
+restartButton = do
+    putStrLn "Deseja reiniciar o programa? (Y / N)"
+    input <- getLine
+
+    case restartCases input of 
+        'Y' -> gameLoop
+        'R' -> restartButton
+        'N' -> putStrLn "Fim do programa, obrigado pela preferência. Ass: CARD-HACK"
+
+
+restartCases :: String -> Char
+restartCases input = 
+    case (map toUpper input) of 
+        "Y" -> 'Y' 
+        "N" -> 'N'
+        _   -> 'R' -- input invalido, refazer
--- card-hack-haskell/src/Components/ProbAlgorithm.hs
+++ card-hack-haskell/src/Components/ProbAlgorithm.hs
@@ -5,6 +5,11 @@ import Data.Ratio as Ratio -- trabalhar precisamente com números em ponto flutu
 import Components.Deck as Deck -- instanciar o mapa que representa o baralho
 import Util.AuxiliarFunctions as Aux -- utilizar funções auxiliares para compor o jogo
 import Data.Maybe (isJust, fromJust) -- trabalhar com Just nos mapas
+import System.Random (randomRIO)
+import System.IO.Unsafe (unsafePerformIO)
+
+randomInt :: Int -> Int -> Int
+randomInt low high = unsafePerformIO $ randomRIO (low, high)
 
 totalCardsInDeck :: Map.Map String Int -> Int
 totalCardsInDeck deck = Deck.howManyCards ((map (\x -> show x) [2..10]) ++ ["J", "K", "Q", "A"]) deck
@@ -44,7 +49,7 @@ underflowProb cards limit deck = do
 		let matchingCards = if maxValue >= 10 then
 								["J", "K", "Q"] ++ (map (\x -> show x) [10,9..2])
 							else
-								(map (\x -> show x) [maxValue,maxValue-1..2])
+								(map (\x -> show x) [maxValue,maxValue-1..2]) ++ ["A"]
 		fromIntegral (Deck.howManyCards matchingCards deck) / fromIntegral (totalCardsInDeck deck)
 
 blackjackProb :: [String] -> Map.Map String Int -> Double 
@@ -61,10 +66,24 @@ blackjackProb cards deck = do
 
 calculateProbs :: Map.Map String [String] -> (Double, Double)
 calculateProbs cardsMap = do
+	let starterDeck = Deck.generateDeck
 	let userCards = fromJust (Map.lookup "user" cardsMap)
 	let dealerCards = fromJust (Map.lookup "dealer" cardsMap)
 	if isBlackjack userCards then
 		(0.0, 1.0)
 	else if isOverflow userCards then
 		(0.0, 0.0)
-	else (1, 2)
+	else do
+		let diff = 21 - (Aux.sumCards userCards) - 1
+		let possibilities = if diff >= 10 then
+								["J", "K", "Q", "A"] ++ (map (\x -> show x) [2..10])
+							else
+								["A"] ++ (map (\x -> show x) [2..diff])
+		let randomIdx = randomInt 0 ((length possibilities) - 1)
+		let randomElement = possibilities !! randomIdx
+		let newUserCards = userCards ++ [randomElement]
+		let newUserSum = Aux.sumCards newUserCards
+		let updatedDeck = Deck.removeCards (userCards ++ dealerCards) starterDeck
+		let probGanharSemPuxar = underflowProb dealerCards (Aux.sumCards userCards) updatedDeck
+		let probGanharPuxando = blackjackProb userCards updatedDeck + (underflowBlackjackProb userCards updatedDeck * underflowProb dealerCards newUserSum (Deck.removeCards [randomElement] updatedDeck))
+		(probGanharPuxando, probGanharSemPuxar)
