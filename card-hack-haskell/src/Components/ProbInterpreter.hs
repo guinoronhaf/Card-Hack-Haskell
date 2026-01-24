cdfef6cf1ff4aa97239bff6609f301cc5c7b489c
joaoneto9 <joaonetocg1@gmail.com>
2026-01-24 10:50:24 -0300

    feat: implementing the ProbInterpreter Archive that shows the probability of the user to win game

--- card-hack-haskell/src/Components/ProbInterpreter.hs
+++ card-hack-haskell/src/Components/ProbInterpreter.hs
@@ -2,46 +2,46 @@ module Components.ProbInterpreter (probAnalise) where
 
 probAnalise :: (Double, Double) -> String
 probAnalise (0.00, 0.00) = unlines 
-    ["OverFlow da quantidade de pontos do usuário, impossível tomar nenhuma ação.",
+    ["OverFlow da quantidade de pontos do usuário, impossível tomar alguma ação.",
     "Probabilidade ao puxar uma carta: 0.0",
     "Probabilidade ao manter as cartas: 0.0"
     ]
 
 probAnalise (x, 0.00) = unlines 
-    ["Pontuação inferior que o Dealer, assim: Observa-se que a probabilidade de vencer sem puxar nenhuma carta é nula.",
+    ["Pontuação inferior que o Dealer, assim: Observa-se que a probabilidade de vencer sem puxar nenhuma carta é nula. Sendo assim, você deve puxar uma carta.",
     "Probabilidade ao puxar uma carta: " ++ (show x),
     "Probabilidade ao manter as cartas: 0.0"
     ]
 
 probAnalise (getCardProb, stayProb)
-    | diff <= 5.00  && diff >= 2.00   =
+    | diff <= 10.00  && diff >= 5.00   =
         unlines
             [ "As probabilidades indicam que apresenta uma grande semelhança nas probabilidades,"
             , "contudo a probabilidade de puxar uma carta é um pouco melhor:"
             , showProbs
             ]
 
-    | diff >= -5.00 && diff <= -2.00  =
+    | diff >= -10.00 && diff <= -5.00  =
         unlines
             [ "As probabilidades indicam que apresenta uma grande semelhança nas probabilidades,"
             , "contudo a probabilidade de não puxar uma carta (stay) é um pouco melhor:"
             , showProbs
             ]
 
-    | diff > -2.00 && diff < 2.00    =
+    | diff > -5.00 && diff < 5.00    =
         unlines
             [ "Praticamente a mesma probabilidade, vai na fé irmão:"
             , showProbs
             ]
 
-    | diff > 5.00                  =
+    | diff > 10.00                  =
         unlines
             [ "Diferença considerável entre as probabilidades,"
             , "indicando que a melhor ação é puxar uma carta:"
             , showProbs
             ]
 
-    | diff < -5.00                 =
+    | diff < -10.00                 =
         unlines
             [ "Diferença considerável entre as probabilidades,"
             , "indicando que a melhor ação é não puxar uma carta (stay):"
